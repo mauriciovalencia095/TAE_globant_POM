cdad0f8d03e73442c343485f52de574d25c14468
valenciv <valenciv@oclc.org>
2024-02-01 15:39:55 -0500

    our first commit

---
+++ page_object_model_baseline-main/src/test/java/co/dlacademy/AirbnbReserve.java
@@ -0,0 +1,25 @@
+package co.dlacademy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AirbnbReserve   {
+    
+    @Test
+    void openPage() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.airbnb.com.co/");
+    }
+}
